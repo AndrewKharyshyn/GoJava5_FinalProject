36ea76446abc6871f808d3821a4d04e68e60d7cb
esalty <esaltydev@gmail.com>
2017-02-02 17:09:29 +0200

    Update Main.java
    Creating controller object

--- FinalProjectCore/Main.java
+++ FinalProjectCore/Main.java
@@ -5,6 +5,7 @@ import FinalProjectCore.Logics.*;
 public class Main {
     public static void main(String[] args) {
         try {
+            Controller controller = new Controller();
             controller.systemEnter();
         } catch (NullPointerException e) {
             System.out.println("Null pointer exception :)");
